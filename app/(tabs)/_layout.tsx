c455cbcde9f31ec93b05973f83d675426505ba75
Zhassulan Baigozha <Zhassulan.Baigozha@nitec.kz>
2025-12-23 17:49:53 +0500

    fix: format code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,8 @@
 import { Tabs } from "expo-router";
-
+import { useColorScheme } from "react-native";
 import { HapticTab } from "@/components/haptic-tab";
 import { IconSymbol } from "@/components/ui/icon-symbol";
 import { Colors } from "@/constants/theme";
-import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,9 +5,9 @@ import {
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
   anchor: "(tabs)",
