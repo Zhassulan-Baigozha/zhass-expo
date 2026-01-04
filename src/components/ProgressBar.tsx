0f06956f36c4b87e6ce643526de3433f89766e70
Zhassulan Baigozha <Zhassulan.Baigozha@nitec.kz>
2026-01-05 02:33:03 +0500

    fix: ProgressBar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -2,19 +2,17 @@ import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, View } from "react-native";
 import { COLORS } from "../constants/colors";
 
-interface ProgressBarProps {
-  /** значение от 0 до 1 */
+interface IProgressBarProps {
   progress: number;
 }
 
-export function ProgressBar({ progress }: Readonly<ProgressBarProps>) {
+export function ProgressBar({ progress }: Readonly<IProgressBarProps>) {
   const clampedProgress = Math.max(0, Math.min(progress, 1));
   console.log('clampedProgress = ', clampedProgress);
 
   return (
     <View style={styles.progressBarWrapper}>
       <View style={styles.progressBarBackground} />
-
       <LinearGradient
         colors={[COLORS.coralShades.coral3, COLORS.coralShades.coral9]}
         start={{ x: 0, y: 0 }}
